Crowder13/sglang-das

[mem_cache][3/N] refactor: move HiSparse allocators to allocator/hisparse.py (#26678)
Co-authored-by: Claude Opus 4.8 (1M context) <noreply@anthropic.com>

python/sglang/srt/model_executor/model_runner_kv_cache_mixin.py
@@ -17,14 +17,14 @@ from sglang.srt.mem_cache.allocator import (
     PagedTokenToKVPoolAllocator,
     TokenToKVPoolAllocator,
 )
+from sglang.srt.mem_cache.allocator.hisparse import (
+    DeepSeekV4HiSparseTokenToKVPoolAllocator,
+    HiSparseTokenToKVPoolAllocator,
+)
 from sglang.srt.mem_cache.allocator.swa import SWATokenToKVPoolAllocator
 from sglang.srt.mem_cache.common import get_req_to_token_extra_context_len
 from sglang.srt.mem_cache.deepseek_v4_memory_pool import DeepSeekV4TokenToKVPool
-from sglang.srt.mem_cache.hisparse_memory_pool import (
-    DeepSeekV4HiSparseTokenToKVPoolAllocator,
-    HiSparseDSATokenToKVPool,
-    HiSparseTokenToKVPoolAllocator,
-)
+from sglang.srt.mem_cache.hisparse_memory_pool import HiSparseDSATokenToKVPool
 from sglang.srt.mem_cache.memory_pool import (
     DSATokenToKVPool,
     HybridLinearKVPool,

test/registered/unit/managers/test_hisparse_unit.py
@@ -94,10 +94,10 @@ class TestHiSparseUnit(unittest.TestCase):
 
         global_page_size = 1 if is_hip() else PAGE_SIZE
 
-        from sglang.srt.mem_cache.hisparse_memory_pool import (
-            HiSparseDSATokenToKVPool,
+        from sglang.srt.mem_cache.allocator.hisparse import (
             HiSparseTokenToKVPoolAllocator,
         )
+        from sglang.srt.mem_cache.hisparse_memory_pool import HiSparseDSATokenToKVPool
 
         cls.device_pool = HiSparseDSATokenToKVPool(
             size=SIZE,

hzwzwzw/sglang

python/sglang/srt/model_executor/model_runner_kv_cache_mixin.py
@@ -17,13 +17,13 @@ from sglang.srt.mem_cache.allocator import (
     PagedTokenToKVPoolAllocator,
     TokenToKVPoolAllocator,
 )
-from sglang.srt.mem_cache.allocator.swa import SWATokenToKVPoolAllocator
-from sglang.srt.mem_cache.deepseek_v4_memory_pool import DeepSeekV4TokenToKVPool
-from sglang.srt.mem_cache.hisparse_memory_pool import (
+from sglang.srt.mem_cache.allocator.hisparse import (
     DeepSeekV4HiSparseTokenToKVPoolAllocator,
-    HiSparseNSATokenToKVPool,
     HiSparseTokenToKVPoolAllocator,
 )
+from sglang.srt.mem_cache.allocator.swa import SWATokenToKVPoolAllocator
+from sglang.srt.mem_cache.deepseek_v4_memory_pool import DeepSeekV4TokenToKVPool
+from sglang.srt.mem_cache.hisparse_memory_pool import HiSparseNSATokenToKVPool
 from sglang.srt.mem_cache.memory_pool import (
     HybridLinearKVPool,
     HybridReqToTokenPool,

test/registered/unit/managers/test_hisparse_unit.py
@@ -90,10 +90,10 @@ class TestHiSparseUnit(unittest.TestCase):
 
         global_page_size = 1 if is_hip() else PAGE_SIZE
 
-        from sglang.srt.mem_cache.hisparse_memory_pool import (
-            HiSparseNSATokenToKVPool,
+        from sglang.srt.mem_cache.allocator.hisparse import (
             HiSparseTokenToKVPoolAllocator,
         )
+        from sglang.srt.mem_cache.hisparse_memory_pool import HiSparseNSATokenToKVPool
 
         cls.device_pool = HiSparseNSATokenToKVPool(
             size=SIZE,